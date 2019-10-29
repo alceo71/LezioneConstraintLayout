b61b09d9d02a8395366ad14bfd9af52a8251b424
DESKTOP-F2URIQD\alceo <giovanni.zorzetti@gmail.com>
2019-10-29 15:23:26 +0100

    Initial commit

---
+++ app/src/main/java/com/example/lezioneconstraintlayout/VirtualHelpersActivity.java
@@ -0,0 +1,14 @@
+package com.example.lezioneconstraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VirtualHelpersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_virtual_helpers);
+    }
+}
